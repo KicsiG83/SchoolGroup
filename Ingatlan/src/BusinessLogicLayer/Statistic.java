ca2450a2df6d7e7e2e425893e45141f80cd1728f
Unknown <balage86620@gmail.com>
2018-07-13 21:09:24 +0200

    ÚLJ_HOZZÁ_TESZT!:

---
+++ Ingatlan/src/BusinessLogicLayer/Statistic.java
@@ -0,0 +1,9 @@
+package BusinessLogicLayer;
+
+public class Statistic {
+	public void getViewCounter() {
+		String query = "SELECT * FROM VIEW_COUNTER_VW";
+	}
+	
+	public  void getSoldStatistic();
+}
